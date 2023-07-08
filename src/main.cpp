670f0be88c138ef2c373c6daac369463bf756d88
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-07-08 15:07:51 +0200

    feat: publish git-remote

--- src/main.cpp
+++ src/main.cpp
@@ -76,6 +76,7 @@ void onConnectionEstablished() {
 		mqttClient.publish(BASE_TOPIC_STATUS "on", String(on), MQTT_RETAINED);
 	});
 
+	mqttClient.publish(BASE_TOPIC "git-remote", GIT_REMOTE, MQTT_RETAINED);
 	mqttClient.publish(BASE_TOPIC "git-version", GIT_VERSION, MQTT_RETAINED);
 	mqttClient.publish(BASE_TOPIC "connected", "1", MQTT_RETAINED);
 	lastConnected = 1;
